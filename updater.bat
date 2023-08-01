2bfce877469c59b993c2709b7429cb370fdf3b32
ThangLC304 <thang.luongcao@gmail.com>
2023-08-01 14:32:48 +0700

    have updater

---
+++ updater.bat
@@ -0,0 +1,82 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "my_repo=https://github.com/ThangLC304/LA3D"
+@REM set my_dir at current directory/temp_update
+set "my_dir=%cd%\temp_update"
+
+@REM Make my_dir if not existed
+if not exist "%my_dir%" mkdir "%my_dir%"
+
+echo "Finding conda path..."
+
+if "%OS%"=="Windows_NT" (
+  set "bat_found=0"
+
+  echo "OS is Windows"
+
+  for %%p in (
+    "C:\ProgramData\miniconda3"
+    "C:\ProgramData\Anaconda3"
+    "C:\ProgramData\.conda"
+    "%UserProfile%\miniconda3"
+    "%UserProfile%\.conda"
+    "%UserProfile%\Anaconda3"
+  ) do (
+      if exist "%%~p\Scripts\activate.bat" (
+          set "conda_path=%%~p"
+          set "bat_found=1"
+      )
+  )
+)
+
+if !bat_found!==0 (
+    echo "No conda installation found. Please install conda and try again."
+    exit /b 1
+) else (
+    echo "Conda installation found at %conda_path%"
+)
+
+echo "Updating program..."
+
+
+
+@REM Check if Git is installed
+where git >nul 2>nul
+if errorlevel 1 (
+  echo Downloading and installing Git...
+  %get_git%
+) else (
+  echo Git is already installed. Skipping installation.
+  FOR /F "tokens=*" %%F IN ('where git') DO SET git_path=%%F
+  echo git path is !git_path!
+)
+
+
+if "%OS%"=="Windows_NT" (
+        set "get_git=call %conda_path%\Scripts\conda install -c anaconda git -y"
+        set clone_cmd=call "!git_path!" clone %my_repo% %my_dir%
+) else (
+        set "get_git=call $conda_path/bin/conda install -c anaconda git -y"
+        set clone_cmd=call "!git_path!" clone %my_repo% %my_dir%
+)
+
+@REM Clone the repo
+echo Cloning the repo...
+%clone_cmd%
+
+@REM copy repo/Libs and repo/main.py to replace current Libs and main.py
+echo Copying Libs/*.* ...
+xcopy /y /q /e "%my_dir%\Libs" "%cd%\Libs"
+echo Copying main.py ...
+xcopy /y /q /e "%my_dir%\main.py" "%cd%\main.py"
+
+@REM Remove temp_update folder
+echo Removing temp_update folder...
+rmdir /s /q "%my_dir%"
+
+echo =================================================================================================
+echo                                    UPDATE COMPLETED !
+echo =================================================================================================
+echo .
+pause
